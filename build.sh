288b12796374fefd64a66b3b48ac669423bdd939
OiSiS <OiSiS@oisis>
2012-08-24 01:06:06 +0200

    Updated build.sh script

--- build.sh
+++ build.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-#export CROSS_COMPILE="/usr/local/arm-2009q3/bin/arm-none-eabi-"
 export CROSS_COMPILE="/home/oisis/Android/cm9/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-"
 export KBUILD_BUILD_VERSION="OiSiS-test-v02"
-#export LOCALVERSION="-G1XXKPN-CL562447"
 
 #which ccache >/dev/null 2>&1
 #if [ $? -eq 0 ]; then
